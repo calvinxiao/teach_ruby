b70ecd97af4263480b9e7e95df04d0f0d3d02891
Calvin Xiao <calvin.xiao@scaurugby.com>
2019-05-20 17:28:20 +0800

    恭喜你发现了本 commit，请于 2019年5月20日 23:59:59 前加 Calvin 微信获取 100 元红包。

--- 0x02.js
+++
@@ -1,4 +0,0 @@
-l = x => x + 1
-console.log(l(1))
-console.log(l.call(null, 1))
-console.log(l.apply(null, [1]))
---
+++ README.md
@@ -0,0 +1,7 @@
+# 友好速搭 Ruby 分享
+
+## 计划 
+
+1. day one, Ruby 基础，`git checkout day1`
+2. Rack 基础 
+
